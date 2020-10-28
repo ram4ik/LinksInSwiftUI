b56f73dc0f19221c53538f0436ec04acedfb1669
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-28 19:37:51 +0200

    Initial Commit

---
+++ LinksInSwiftUI/LinksInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinksInSwiftUIApp.swift
+//  LinksInSwiftUI
+//
+//  Created by Ramill Ibragimov on 28.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct LinksInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
